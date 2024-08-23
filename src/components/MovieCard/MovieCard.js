bc088705b0da5bbac6f334413fe1bdbd297e5372
Seth Way <way.sethaniel@gmail.com>
2024-08-23 14:02:35 -0400

    finish movie card styling Co-authored-by: Marshall Hotaling marshall.hotaling@gmail.com

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,4 +1,5 @@
 import './MovieCard.css';
+import turnip from '../../assets/images/turnip.png';
 import { ReactComponent as TurnipSVG } from '../../assets/images/turnip.svg';
 
 export default function MovieCard({ movie }) {
@@ -9,13 +10,18 @@ export default function MovieCard({ movie }) {
   };
   return (
     <div className='movie-card' onClick={handleClick}>
-      <div className='img-wrapper'>
-        <img src={poster_path} alt={`${title} movie poster`} />
+      <div className='card-inside'>
+        <div className='img-wrapper'>
+          <img src={poster_path} alt={`${title} movie poster`} />
+        </div>
+        <div className='rating'>
+          <img src={turnip} alt='turnip icon' />
+          <p> {average_rating.toFixed(1)}</p>
+        </div>
+        <div className='title'>
+          <h3>{title}</h3>
+        </div>
       </div>
-      <div className='rating'>
-        <TurnipSVG /> <p>{average_rating.toFixed(1)}</p>
-      </div>
-      <h3>{title}</h3>
     </div>
   );
 }
